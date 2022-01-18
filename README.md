70e7cd6038081033ce96b45d8520df7a26743aa0
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2022-01-18 16:06:17 +0000

    Deploy the KBV Experian API to PaaS

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # di-cri-experian-kbv-api
 API used by Credential Issuers to connect to Experian KBV APIs
+
+## Deploy to PaaS
+
+Set environment variables with:
+````
+cf set-env di-cri-experian-kbv-api KEYSTORE_PASSWORD <the password>
+cf set-env di-cri-experian-kbv-api KEYSTORE $(base64 < ~/path/to/keystore)
+````
+
+Then build and push with:
+````
+./gradlew clean build
+cf push
+````
--- src/main/java/uk/gov/di/cri/experian/kbv/api/App.java
+++ src/main/java/uk/gov/di/cri/experian/kbv/api/App.java
@@ -1,8 +1,17 @@
 package uk.gov.di.cri.experian.kbv.api;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+import java.util.UUID;
+
 public class App {
-    public static void main(String[] args) {
-        System.setProperty("javax.net.ssl.keyStore", System.getenv("KEYSTORE_PATH"));
+    public static void main(String[] args) throws IOException {
+        String keystoreBase64 = System.getenv("KEYSTORE");
+        Path tempFile = Files.createTempFile(UUID.randomUUID().toString(), null);
+        Files.write(tempFile, Base64.getDecoder().decode(keystoreBase64));
+        System.setProperty("javax.net.ssl.keyStore", tempFile.toString());
         System.setProperty("javax.net.ssl.keyStorePassword", System.getenv("KEYSTORE_PASSWORD"));
 
         new ExperianApi();
